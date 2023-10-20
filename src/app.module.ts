8125d3a4a43f0b2a2a0a15df1de1f35bafcadac5
Kateryna Kleshch <ketrins1993@gmail.com>
2023-10-20 22:14:50 +0300

    add resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DealsModule } from './deals/deals.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, DealsModule],
   controllers: [AppController],
   providers: [AppService],
 })
