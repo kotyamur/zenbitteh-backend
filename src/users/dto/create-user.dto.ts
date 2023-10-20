18dafe412f839de4a8ad46779f7069f8cec60d7d
Kateryna Kleshch <ketrins1993@gmail.com>
2023-10-20 23:11:58 +0300

    add user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @MinLength(6, {
+    message: 'Password must be longer than or equal to 6 symbols',
+  })
+  @MaxLength(10, {
+    message: 'Password is too long. Maximal length is 10 symbols',
+  })
+  password: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({ type: 'int' })
+  id: number;
+
+  @Column({ type: 'varchar' })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+}
